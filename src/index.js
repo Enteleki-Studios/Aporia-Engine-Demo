9207d71d11d87a0eeb83478ccf2e33235b82c2bc
mike <mikeymaxdb@users.noreply.github.com>
2021-06-16 19:07:12 -0700

    more setup, loaded first model

--- src/index.js
+++ src/index.js
@@ -1,1 +1,144 @@
-console.debug('test')
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+
+import 'style/root.scss'
+
+
+class BasicWorldDemo {
+    constructor() {
+        this._Initialize()
+    }
+
+    _Initialize() {
+        this._threejs = new THREE.WebGLRenderer({
+            antialias: true,
+            canvas: document.getElementById('WebGLCanvas'),
+        })
+        this._threejs.outputEncoding = THREE.sRGBEncoding
+        this._threejs.gammaFactor = 2.2
+        this._threejs.shadowMap.enabled = true
+        this._threejs.shadowMap.type = THREE.PCFSoftShadowMap
+        this._threejs.setPixelRatio(window.devicePixelRatio)
+
+        window.addEventListener('resize', () => {
+            this._OnWindowResize()
+        }, false)
+
+        const fov = 60
+        const aspect = 1920 / 1080
+        const near = 1.0
+        const far = 1000.0
+        this._camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
+        this._camera.position.set(75, 20, 0)
+        this._OnWindowResize()
+
+        this._scene = new THREE.Scene()
+
+        const light = new THREE.DirectionalLight(0xFFFFFF, 1.0)
+        light.position.set(20, 100, 10)
+        light.target.position.set(0, 0, 0)
+        light.castShadow = true
+        light.shadow.bias = -0.001
+        light.shadow.mapSize.width = 2048
+        light.shadow.mapSize.height = 2048
+        light.shadow.camera.near = 0.1
+        light.shadow.camera.far = 500.0
+        light.shadow.camera.near = 0.5
+        light.shadow.camera.far = 500.0
+        light.shadow.camera.left = 100
+        light.shadow.camera.right = -100
+        light.shadow.camera.top = 100
+        light.shadow.camera.bottom = -100
+        this._scene.add(light)
+
+        const ambientLight = new THREE.AmbientLight(0x101010, 10)
+        this._scene.add(ambientLight)
+
+        const controls = new OrbitControls(
+            this._camera, this._threejs.domElement)
+        controls.target.set(0, 20, 0)
+        controls.update()
+
+        const loader = new THREE.CubeTextureLoader()
+        const texture = loader.load([
+            '/resources/skybox/posx.jpg',
+            '/resources/skybox/negx.jpg',
+            '/resources/skybox/posy.jpg',
+            '/resources/skybox/negy.jpg',
+            '/resources/skybox/posz.jpg',
+            '/resources/skybox/negz.jpg',
+        ])
+        this._scene.background = texture
+
+        const plane = new THREE.Mesh(
+            new THREE.PlaneGeometry(100, 100, 10, 10),
+            new THREE.MeshStandardMaterial({
+                color: 0x6272a4,
+            }))
+        plane.castShadow = false
+        plane.receiveShadow = true
+        plane.rotation.x = -Math.PI / 2
+        this._scene.add(plane)
+
+        // const box = new THREE.Mesh(
+        //     new THREE.BoxGeometry(2, 2, 2),
+        //     new THREE.MeshStandardMaterial({
+        //         color: 0xFFFFFF,
+        //     }))
+        // box.position.set(0, 1, 0)
+        // box.castShadow = true
+        // box.receiveShadow = true
+        // this._scene.add(box)
+
+        this._LoadAnimatedModel()
+
+        this.tick()
+    }
+    _LoadAnimatedModel() {
+        const loader = new FBXLoader()
+        loader.setPath('./resources/models/')
+        loader.load('Rogue.fbx', (fbx) => {
+            console.debug(fbx)
+
+            this._scene.add(fbx)
+
+            fbx.scale.setScalar(0.1)
+
+            const textureLoader = new THREE.TextureLoader()
+            const texture = textureLoader.load('./resources/models/Textures/Rogue_Texture.png')
+            texture.encoding = THREE.sRGBEncoding
+            texture.flipY = true
+
+            fbx.traverse((c) => {
+                c.castShadow = true
+                c.receiveShadow = true
+                if (c.material) {
+                    c.material.map = texture
+                    c.material.side = THREE.DoubleSide
+                }
+            })
+        })
+    }
+
+    _OnWindowResize() {
+        const width = this._threejs.domElement.width
+        const height = this._threejs.domElement.height
+        this._camera.aspect = width / height
+        this._camera.updateProjectionMatrix()
+        this._threejs.setSize(width, height)
+    }
+
+    tick() {
+        requestAnimationFrame(() => {
+            this._threejs.render(this._scene, this._camera)
+            this.tick()
+        })
+    }
+}
+
+let _APP = null
+
+window.addEventListener('DOMContentLoaded', () => {
+    _APP = new BasicWorldDemo()
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,40 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
 module.exports = {
     entry: './src/index.js',
-    plugins: [new HtmlWebpackPlugin()],
+    resolve: {
+        modules: [
+            path.resolve(__dirname, 'node_modules'),
+            path.resolve(__dirname, 'src'),
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'index',
+            template: 'src/index.html',
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'resources', to: 'resources' },
+            ],
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         port: 1080,
