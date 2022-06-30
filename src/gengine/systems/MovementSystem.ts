3b8829fab3e693fa259bfa74a1ae9be97c20906a
mike <mikeymaxdb@users.noreply.github.com>
2022-06-29 18:42:44 -0700

    redo camera and movement systems, update deps

--- src/gengine/systems/MovementSystem.ts
+++ src/gengine/systems/MovementSystem.ts
@@ -5,11 +5,10 @@ import { PositionComponent } from '../components/PositionComponent'
 import { VelocityComponent } from '../components/VelocityComponent'
 import { InputComponent } from '../components/InputComponent'
 import { CameraComponent } from '../components/CameraComponent'
-import { Y_AXIS } from '../constants'
+import { X_AXIS, Y_AXIS } from '../constants'
 import { World } from '../World'
 
 const deceleration = new Vector3(-5, -0.0001, -5)
-const acceleration = new Vector3(15, 0.01, 15)
 
 const RUN_BOOST = 2
 const ROTATION_SPEED = 8
@@ -31,46 +30,49 @@ export class MovementSystem extends System {
             const positionComponent = entity.get(PositionComponent)
             const velocityComponent = entity.get(VelocityComponent)
 
-            const { direction } = cameraEntity.get(CameraComponent)
+            const { position: camPosition } = cameraEntity.get(CameraComponent)
+            const cameraDirection = new Vector3().subVectors(camPosition, positionComponent.position)
             const { velocity } = velocityComponent
 
             // Calculate deceleration
             const frameDeceleration = velocity.clone().multiply(deceleration).multiplyScalar(delta)
 
-            // Calculate acceleration
-            // TODO diagonal movement shouldn't be faster
-            const frameAcceleration = new Vector3()
-            if (inputComponent.input.up.hold) {
-                frameAcceleration.z += acceleration.z
-            }
-            if (inputComponent.input.down.hold) {
-                frameAcceleration.z -= acceleration.z
-            }
-            if (inputComponent.input.left.hold) {
-                frameAcceleration.x += acceleration.x
-            }
-            if (inputComponent.input.right.hold) {
-                frameAcceleration.x -= acceleration.x
-            }
-
+            // Accelerate away from the camera
+            const frameAcceleration = cameraDirection.clone().setY(0).negate().normalize()
+            const camAngle = frameAcceleration.angleTo(X_AXIS)
             const boost = inputComponent.input.run.hold ? RUN_BOOST : 1
-            frameAcceleration.multiplyScalar(delta)
-            frameAcceleration.setLength(frameAcceleration.length() * boost)
 
-            // Apply acceleration to velocity
-            const forward = new Vector3(0, 0, 1)
-            forward.applyQuaternion(direction)
-            forward.normalize()
-            forward.multiplyScalar(frameAcceleration.z)
+            let targetAngle = 0
+            if (inputComponent.input.up.hold) {
+                if (inputComponent.input.left.hold) {
+                    targetAngle = Math.PI / 4
+                } else if (inputComponent.input.right.hold) {
+                    targetAngle = Math.PI / -4
+                }
+            } else if (inputComponent.input.down.hold) {
+                if (inputComponent.input.left.hold) {
+                    targetAngle = (Math.PI / 4) * 3
+                } else if (inputComponent.input.right.hold) {
+                    targetAngle = (Math.PI / 4) * -3
+                } else {
+                    targetAngle = Math.PI
+                }
+            } else if (inputComponent.input.left.hold) {
+                targetAngle = Math.PI / 2
+            } else if (inputComponent.input.right.hold) {
+                targetAngle = Math.PI / -2
+            } else {
+                // No input, no acceleration
+                frameAcceleration.multiplyScalar(0)
+            }
 
-            const sideways = new Vector3(1, 0, 0)
-            sideways.applyQuaternion(direction)
-            sideways.normalize()
-            sideways.multiplyScalar(frameAcceleration.x)
+            // Rotate acceleration in the direction of travel
+            frameAcceleration.applyAxisAngle(Y_AXIS, targetAngle)
 
-            velocity.add(frameDeceleration)
-            velocity.add(forward)
-            velocity.add(sideways)
+            // Set acceleration magnitude
+            frameAcceleration.multiplyScalar(15 * delta * boost)
+
+            velocity.add(frameDeceleration).add(frameAcceleration)
 
             // Rotate model to match input
             const targetRotation = positionComponent.rotation.clone()
@@ -81,30 +83,11 @@ export class MovementSystem extends System {
             || inputComponent.input.left.hold
             || inputComponent.input.right.hold
             ) {
-                // Reset rotation to face direction
-                targetRotation.copy(direction)
+                // Reset rotation to face away from camera
+                targetRotation.setFromAxisAngle(Y_AXIS, camAngle)
             }
 
-            let targetAngle = 0
-            if (inputComponent.input.up.hold) {
-                if (inputComponent.input.left.hold) {
-                    targetAngle = Math.PI / 4
-                } else if (inputComponent.input.right.hold) {
-                    targetAngle = Math.PI / -4
-                }
-            } else if (inputComponent.input.down.hold) {
-                targetAngle = Math.PI
-                if (inputComponent.input.left.hold) {
-                    targetAngle = (Math.PI / 4) * 3
-                } else if (inputComponent.input.right.hold) {
-                    targetAngle = (Math.PI / 4) * -3
-                }
-            } else if (inputComponent.input.left.hold) {
-                targetAngle = Math.PI / 2
-            } else if (inputComponent.input.right.hold) {
-                targetAngle = Math.PI / -2
-            }
-
+            // Rotate towards movement direction
             Q.setFromAxisAngle(Y_AXIS, targetAngle)
             targetRotation.multiply(Q)
 
--- src/gengine/systems/ThirdPersonCameraSystem.ts
+++ src/gengine/systems/ThirdPersonCameraSystem.ts
@@ -1,14 +1,12 @@
-import { Quaternion, Vector3 } from 'three'
+import { Vector3 } from 'three'
 import { PositionComponent } from '../components/PositionComponent'
 import { CameraComponent } from '../components/CameraComponent'
 import { InputComponent } from '../components/InputComponent'
 import { CameraTargetComponent } from '../components/CameraTargetComponent'
-import { Y_AXIS } from '../constants'
 import { System } from '../ECS/System'
 import { World } from '../World'
 import { ECSFilter } from '../ECS/ECSFilter'
 
-const forwardQ = new Quaternion()
 const camPosition = new Vector3()
 
 export class ThirdPersonCameraSystem extends System {
@@ -16,7 +14,6 @@ export class ThirdPersonCameraSystem extends System {
     cameraFilter = new ECSFilter([CameraComponent, InputComponent])
 
     filters = [this.cameraTargetFilter, this.cameraFilter]
-    // const cameraTargets = componentManager.getTuplesByClass(CameraTargetComponent, PositionComponent)
 
     tick(world: World) {
         const delta = world.timeElapsedS
@@ -24,27 +21,20 @@ export class ThirdPersonCameraSystem extends System {
 
         this.cameraFilter.entities.forEach((cameraEntity) => {
             const cameraComponent = cameraEntity.get(CameraComponent)
-            const inputComponent = cameraEntity.get(InputComponent)
             const { position: targetPosition } = cameraTargets[0].get(PositionComponent)
 
-            // Update forwards direction
-            // Set Q to difference in direction
-            forwardQ.setFromAxisAngle(Y_AXIS, -2 * Math.PI * inputComponent.mouse.pan.x * delta * 0.01)
-            // Apply difference to component direction
-            cameraComponent.direction.multiply(forwardQ)
+            camPosition.copy(targetPosition)
 
-            camPosition.set(0, 0, 1)
-            // camPosition.copy(position)
-            camPosition.applyQuaternion(cameraComponent.direction) // Point camera forward
-            camPosition.multiplyScalar(-5) // Move it back
-            camPosition.y += 5 // Move it up
-            camPosition.add(targetPosition) // Move it into position
+            camPosition.x -= 5
+            camPosition.z -= 7
+            camPosition.y += 7
 
             cameraComponent.position.lerp(camPosition, 1 - (0.005 ** delta))
             cameraComponent.lookAt.lerp(targetPosition, 1 - (0.00001 ** delta))
             // cameraComponent.position.copy(camPosition)
             // cameraComponent.lookAt.copy(targetPosition)
-            cameraComponent.lookAt.setY(2) // Look a little higher
         })
     }
 }
+// Panning
+// forwardQ.setFromAxisAngle(Y_AXIS, -2 * Math.PI * inputComponent.mouse.pan.x * delta * 0.01)
