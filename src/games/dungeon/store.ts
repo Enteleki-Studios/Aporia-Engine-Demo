5d0cd3ac2764907ab9890404897d9a2d6986c107
mike <mikeymaxdb@users.noreply.github.com>
2022-07-03 17:33:08 -0700

    turn off logger middleware

--- src/games/dungeon/store.ts
+++ src/games/dungeon/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import logger from 'redux-logger'
+// import logger from 'redux-logger'
 
 import { rootReducer } from 'models/reducers'
 
@@ -7,7 +7,10 @@ import { middleware as dungeonMiddleware } from './dungeon'
 
 export const store = configureStore({
     reducer: rootReducer,
-    middleware: [dungeonMiddleware, logger],
+    middleware: [
+        dungeonMiddleware,
+        // logger,
+    ],
 })
 
 export type RootState = ReturnType<typeof rootReducer>
