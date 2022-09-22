03a69059c1c51ed6927699d5995c3a84eeac595c
mike <mikeymaxdb@users.noreply.github.com>
2022-09-21 18:17:06 -0700

    added jest

---
+++ src/gengine/Inspector/tests/utils.test.ts
@@ -0,0 +1,48 @@
+import { secondsToClockString } from '../utils'
+
+describe('secondsToClockString', () => {
+    describe('with no decimal places', () => {
+        describe.each([
+            [0, '00:00:00'],
+            [5, '00:00:05'],
+            [55, '00:00:55'],
+            [60, '00:01:00'],
+            [90, '00:01:30'],
+            [60 * 10, '00:10:00'],
+            [60 * 60, '01:00:00'],
+            [(60 * 60) + (60 * 32) + (7), '01:32:07' ],
+        ] as const)('%d seconds', (seconds, expected) => {
+            it(`returns ${expected}`, () => {
+                expect(secondsToClockString(seconds)).toBe(expected)
+            })
+        })
+    })
+
+    describe('with decimal places', () => {
+        describe.each([
+            [0, 0, '00:00:00'],
+            [0, 1, '00:00:00.0'],
+            [0, 2, '00:00:00.00'],
+            [0, 3, '00:00:00.000'],
+            [5.123, 1, '00:00:05.1'],
+            [5.123, 2, '00:00:05.12'],
+            [5.123, 3, '00:00:05.123'],
+            [(60 * 60) + (60 * 32) + (7.413), 1, '01:32:07.4' ],
+            [(60 * 60) + (60 * 32) + (7.493), 1, '01:32:07.5' ], // rounds
+            [(60 * 60) + (60 * 32) + (7.142), 2, '01:32:07.14' ],
+            [(60 * 60) + (60 * 32) + (7.149), 2, '01:32:07.15' ], // rounds
+            [(60 * 60) + (60 * 32) + (7.789), 3, '01:32:07.789' ],
+            [5, 0, '00:00:05'],
+            [55, 0, '00:00:55'],
+            [60, 0, '00:01:00'],
+            [90, 0, '00:01:30'],
+            [60 * 10, 0, '00:10:00'],
+            [60 * 60, 0, '01:00:00'],
+            [(60 * 60) + (60 * 32) + (7), 0, '01:32:07' ],
+        ] as const)('%d seconds, %i decimal places', (seconds, decimalPlaces, expected) => {
+            it(`returns ${expected}`, () => {
+                expect(secondsToClockString(seconds, decimalPlaces)).toBe(expected)
+            })
+        })
+    })
+})
--- src/gengine/Inspector/utils.ts
+++ src/gengine/Inspector/utils.ts
@@ -6,31 +6,3 @@ export const secondsToClockString = (seconds: number, decimalPlaces = 0) => {
 
     return `${hours}:${minutes}:${secs}`
 }
-
-// TODO move this to actual tests
-const testSecondsToClockString = () => {
-    console.debug('Testing secondsToClockString()')
-
-    const tests = [
-        ['00:00:05', 5],
-        ['00:00:55', 55],
-        ['00:01:00', 60],
-        ['00:01:30', 90],
-        ['00:10:00', 60 * 10],
-        ['01:00:00', 60 * 60],
-        ['01:32:07', (60 * 60) + (60 * 32) + (7)],
-    ] as const
-
-    tests.forEach((t) => {
-        const output = secondsToClockString(t[1])
-
-        console.assert(
-            output === t[0],
-            t[1],
-            `wanted: ${t[0]}`,
-            `got: ${output}`,
-        )
-    })
-}
-
-testSecondsToClockString()
