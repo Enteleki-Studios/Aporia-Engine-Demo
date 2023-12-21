0a17394da2895a5790d539acacb6e0244fbb15b8
mike <mikeymaxdb@users.noreply.github.com>
2023-12-20 17:40:01 -0800

    plugin type tweaks

--- packages/gengine/src/core/createPlugin.ts
+++ packages/gengine/src/core/createPlugin.ts
@@ -1,15 +1,22 @@
 import { type World } from 'core'
 
+type Unnamed<T> = Omit<T, 'name'>
+type Named<T> = T & { name: string }
+
 export type Plugin = {
     name: string
     init(world: World): void
 }
 
-type PreparePlugin<P extends Plugin, T = void> = (options: T) => P
+type PreparePlugin<P extends Unnamed<Plugin>, T = void> = (options: T) => P
 
-export const createPlugin = <P extends Plugin, T = void>(preparePlugin: PreparePlugin<P, T>) => {
-    const pluginCreator = (options: T): P => {
-        return preparePlugin(options)
+export const createPlugin = <P extends Unnamed<Plugin>, T = void>(name: string, preparePlugin: PreparePlugin<P, T>) => {
+    const pluginCreator = (options: T): Named<P> => {
+        const plugin = preparePlugin(options) as P & { name: string }
+
+        plugin.name = name
+
+        return plugin
     }
 
     return pluginCreator
--- packages/gengine/src/plugins/threejs/index.ts
+++ packages/gengine/src/plugins/threejs/index.ts
@@ -1,9 +1,7 @@
-import { Group, Object3D } from 'three'
 import { Octree } from 'three/examples/jsm/math/Octree'
 import { OctreeHelper } from 'three/examples/jsm/helpers/OctreeHelper'
 
-import { createPlugin, createSystem, World } from 'core'
-import { ResourceManager } from 'managers/ResourceManager'
+import { createPlugin, World } from 'core'
 import {
     directionalLightFilter,
     cameraFilter,
@@ -16,76 +14,12 @@ import {
     mesh2DFilter,
 } from 'filters'
 
-import {
-    directionalLightComponent,
-    cameraComponent,
-    positionComponent,
-    directionComponent,
-} from 'components'
-
 import { Renderer } from './Renderer'
 import { makeObject3dManager, makeAnimationManager } from './object3dManager'
 import { entityReceiver } from './entityReceiver'
+import { rendererSystem } from './systems'
 
-const LABEL = 'render'
-export const rendererSystem = createSystem<{ renderer: Renderer; objectManager: ResourceManager<Group, Object3D> }>(
-    LABEL,
-    ({ renderer, objectManager }) =>
-        (world: World) => {
-            // world.ecs.filterBy(modelFilter).forEach((entity) => {
-            //     if (entity.has(healthComponent)) {
-            //         this.updateHealthIndicator(entity)
-            //     }
-            // })
-
-            world.ecs.filterBy(directionalLightFilter).forEach((entity) => {
-                const { position, target } = entity.get(directionalLightComponent)
-                objectManager.getResource(entity.id, 'directionalLight')?.position.fromArray(position)
-                objectManager.getResource(entity.id, 'directionalLightTarget')?.position.fromArray(target)
-            })
-
-            world.ecs.filterBy(cameraFilter).forEach((entity) => {
-                const { position, lookAt } = entity.get(cameraComponent)
-                renderer.camera.position.fromArray(position)
-                renderer.camera.lookAt(...lookAt)
-            })
-
-            world.ecs.filterBy(movingEntitiesFilter).forEach((entity) => {
-                const { position } = entity.get(positionComponent)
-                objectManager.getContainer(entity.id)?.position.fromArray(position)
-            })
-
-            // TODO only do this for dirty entities/components
-            world.ecs.filterBy(rotatingEntitiesFilter).forEach((entity) => {
-                // TODO this if statement is a hack...
-                // if (!entity.hasTag(tags.hero)) {
-                const { position } = entity.get(positionComponent)
-                const { direction } = entity.get(directionComponent)
-
-                objectManager
-                    .getContainer(entity.id)
-                    ?.lookAt(position[0] + direction[0], position[1] + direction[1], position[2] + direction[2])
-                // }
-            })
-
-            renderer.render()
-
-            world.stats.systemsStats[LABEL].extra.calls = renderer.renderer.info.render.calls
-            world.stats.systemsStats[LABEL].extra.triangles = renderer.renderer.info.render.triangles
-            world.stats.systemsStats[LABEL].extra.geometries = renderer.renderer.info.memory.geometries
-            world.stats.systemsStats[LABEL].extra.textures = renderer.renderer.info.memory.textures
-
-            const shaders = renderer.renderer.info.programs
-            if (shaders) {
-                world.stats.systemsStats[LABEL].extra.shaders = shaders.length
-            }
-
-            // TODO move this to renderer core
-            renderer.renderer.info.reset()
-        },
-)
-
-export const threejsPlugin = createPlugin(() => {
+export const threejsPlugin = createPlugin('Three.js plugin', () => {
     const renderer = new Renderer({})
     const objectManager = makeObject3dManager(renderer)
     const animationManager = makeAnimationManager()
@@ -98,10 +32,10 @@ export const threejsPlugin = createPlugin(() => {
     const threeEntityReceiver = entityReceiver({ renderer, objectManager, octree, octreeHelper, animationManager })
 
     return {
-        name: 'Three.js Plugin',
         init(world: World) {
             world.registerSystem(rendererSystem({ renderer, objectManager }))
 
+            // TODO remove need to register filters
             world.ecs.registerFilters([
                 directionalLightFilter,
                 cameraFilter,
