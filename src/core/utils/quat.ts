7544b21cd1b65717a93208f81bf56d9c7bfd10a7
mike <mikeymaxdb@users.noreply.github.com>
2025-12-06 13:47:12 -0800

    slightly more efficient quatLookAt

--- src/core/utils/quat.ts
+++ src/core/utils/quat.ts
@@ -1,11 +1,12 @@
-import { type mat3, quat, vec3 } from 'gl-matrix'
+import { mat3, quat, vec3 } from 'gl-matrix'
 
 const Y_AXIS = [0, 1, 0]
 
-// Vector pool
+// Object pool
 const x: vec3 = [0, 0, 0]
 const y: vec3 = [0, 0, 0]
 const z: vec3 = [0, 0, 0]
+const m: mat3 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 // TODO: May fail if dir is parallel to UP
 
@@ -25,11 +26,12 @@ export const quatLookAt = (out: quat, dir: vec3, up: vec3 = Y_AXIS): quat => {
 
     // Build rotation matrix
     // prettier-ignore
-    const m: mat3 = [
+    mat3.set(
+        m,
         x[0], x[1], x[2],
         y[0], y[1], y[2],
         z[0], z[1], z[2],
-    ]
+    )
 
     quat.fromMat3(out, m)
 
