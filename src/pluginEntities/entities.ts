7be1d576b51e7be02a988176f3fe42badbc468e9
mike <mikeymaxdb@users.noreply.github.com>
2025-12-28 17:56:05 -0800

    finished moving logic to QueryCache

--- src/pluginEntities/entities.ts
+++ src/pluginEntities/entities.ts
@@ -1,109 +1,13 @@
-/* eslint-disable @typescript-eslint/no-explicit-any -- library code */
-/* eslint-disable @typescript-eslint/consistent-type-assertions -- library code */
-import { type AnyComponent, type AnyComponentCreator, ObjectStore } from '@core'
+import { type AnyComponent, type AnyComponentCreator } from '@core'
 
 import { Entity, type EntityId } from './entity'
-import { type Query, entityMatchesQuery } from './query'
-
-type ComponentsFromCreators<T extends readonly AnyComponentCreator[]> = {
-    [K in keyof T]: T[K] extends AnyComponentCreator ? ReturnType<T[K]> : never
-}
-
-type QueryResult<
-    T extends readonly AnyComponentCreator[] = readonly AnyComponentCreator[],
-> = [ComponentsFromCreators<T>, Entity]
-
-type QueryEffectCleanup = () => void
-
-type QueryEffect<
-    T extends readonly AnyComponentCreator[] = readonly AnyComponentCreator[],
-> = (result: QueryResult<T>) => void | null | QueryEffectCleanup
-
-type QueryCacheEntry<
-    T extends readonly AnyComponentCreator[] = readonly AnyComponentCreator[],
-> = {
-    results: QueryResult<T>[]
-    effects: QueryEffect<T>[]
-    cleanups: (null | (() => void))[]
-}
-type QueryCache = ObjectStore<Query<any>, QueryCacheEntry>
-
-const getComponentsAsTuple = <T extends readonly AnyComponentCreator[]>(
-    entity: Entity,
-    requires: T,
-): ComponentsFromCreators<T> =>
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- TODO: Refactor to remove this
-    requires.map((r) => entity.get(r)!) as any as ComponentsFromCreators<T>
-
-const entityToQueryResult = <T extends readonly AnyComponentCreator[]>(
-    entity: Entity,
-    query: Query<T>,
-): QueryResult<T> => [getComponentsAsTuple(entity, query.requires), entity]
+import { type Query } from './query'
+import { type QueryResult, type QueryEffect, QueryCache } from './queryCache'
 
 export class Entities {
     private entities = new Map<EntityId, Entity>()
-    private queryCache: QueryCache = new ObjectStore(() => ({
-        results: [],
-        effects: [],
-        cleanups: [],
-    }))
+    private queryCache = new QueryCache(this.entities)
 
-    private updateQueryResultsForEntity(entity: Entity) {
-        this.queryCache.forEach((cacheEntry, query) => {
-            const { results, effects, cleanups } = cacheEntry
-
-            const resultIndex = results.findIndex((res) => res[1] === entity)
-            // TODO: Adding an effect after cleanups have been created
-            // will change the value of effects.length and mess up all
-            // of the offsets used to find cleanups. Consider saving cleanups
-            // with effects using tuples instead of as an array sibling
-            if (entityMatchesQuery(entity, query)) {
-                // Entity matches for the first time
-                if (resultIndex < 0) {
-                    const newResult = entityToQueryResult(entity, query)
-                    results.push(newResult)
-                    const newResultIndex = results.length
-                    const cleanupOffset = newResultIndex * effects.length
-                    effects.forEach((effect, i) => {
-                        const cleanupIndex = cleanupOffset + i
-                        const cleanup = effect(newResult)
-                        cleanups[cleanupIndex] = cleanup ?? null
-                    })
-                }
-                // Entity already matched
-                else {
-                    const result = results[resultIndex]
-
-                    if (result) {
-                        // Always true
-                        const cleanupOffset = resultIndex * effects.length
-                        effects.forEach((effect, i) => {
-                            // Run corresponding cleanup first
-                            const cleanupIndex = cleanupOffset + i
-                            cleanups[cleanupIndex]?.()
-
-                            // Call the effect and save the cleanup function
-                            const cleanup = effect(result)
-                            cleanups[cleanupIndex] = cleanup ?? null
-                        })
-                    }
-                }
-            } else {
-                // Entity needs to be removed from results
-                if (resultIndex >= 0) {
-                    results.splice(resultIndex, 1)
-                    const removedCleanups = cleanups.splice(
-                        resultIndex * effects.length,
-                        effects.length,
-                    )
-                    removedCleanups.forEach((cleanup) => {
-                        cleanup?.()
-                    })
-                }
-                // Ignore Entities that do not and did not match
-            }
-        })
-    }
     get(entityId: EntityId) {
         return this.entities.get(entityId)
     }
@@ -111,6 +15,7 @@ export class Entities {
     createEntity() {
         const entity = new Entity()
         this.entities.set(entity.id, entity)
+        this.queryCache.onEntityUpdated(entity)
         return entity.id
     }
 
@@ -120,7 +25,7 @@ export class Entities {
             components.forEach((c) => {
                 entity.add(c)
             })
-            this.updateQueryResultsForEntity(entity)
+            this.queryCache.onEntityUpdated(entity)
         }
     }
 
@@ -130,26 +35,12 @@ export class Entities {
             componentCreators.forEach((c) => {
                 entity.delete(c)
             })
-            this.updateQueryResultsForEntity(entity)
+            this.queryCache.onEntityUpdated(entity)
         }
     }
 
     query<T extends readonly AnyComponentCreator[]>(query: Query<T>): QueryResult<T>[] {
-        const cacheEntry = this.queryCache.get(query)
-
-        if (cacheEntry) {
-            return cacheEntry.results as QueryResult<T>[]
-        }
-
-        const results = this.entities
-            .values()
-            .filter((entity) => entityMatchesQuery(entity, query))
-            .map((entity) => entityToQueryResult(entity, query))
-            .toArray()
-
-        this.queryCache.create(query).results = results as QueryResult<any>[]
-
-        return results
+        return this.queryCache.query(query)
     }
 
     queryFirst<T extends readonly AnyComponentCreator[]>(
@@ -162,9 +53,7 @@ export class Entities {
         query: Query<T>,
         effect: QueryEffect<T>,
     ) {
-        const [cacheEntry] = this.queryCache.getOrCreate(query)
-
-        ;(cacheEntry as QueryCacheEntry<T>).effects.push(effect)
+        this.queryCache.addQueryEffect(query, effect)
     }
 
     get size() {
@@ -175,6 +64,3 @@ export class Entities {
         return this.queryCache.size
     }
 }
-
-/* eslint-enable @typescript-eslint/no-explicit-any -- library code */
-/* eslint-enable @typescript-eslint/consistent-type-assertions -- library code */
---
+++ src/pluginEntities/queryCache.ts
@@ -0,0 +1,130 @@
+/* eslint-disable @typescript-eslint/no-explicit-any -- library code */
+/* eslint-disable @typescript-eslint/consistent-type-assertions -- library code */
+import { Entity, EntityId } from './entity'
+import { type Query, entityMatchesQuery } from './query'
+import { type AnyComponentCreator } from '@core'
+
+type ComponentsFromCreators<T extends readonly AnyComponentCreator[]> = {
+    [K in keyof T]: T[K] extends AnyComponentCreator ? ReturnType<T[K]> : never
+}
+
+export type QueryResult<
+    T extends readonly AnyComponentCreator[] = readonly AnyComponentCreator[],
+> = [ComponentsFromCreators<T>, Entity]
+
+type QueryEffectCleanup = () => void
+
+export type QueryEffect<
+    T extends readonly AnyComponentCreator[] = readonly AnyComponentCreator[],
+> = (result: QueryResult<T>) => void | null | QueryEffectCleanup
+
+type QueryCacheEntry<
+    T extends readonly AnyComponentCreator[] = readonly AnyComponentCreator[],
+> = {
+    results: QueryResult<T>[]
+    effects: [QueryEffect<T>, (QueryEffectCleanup | null)[]][]
+}
+
+const getComponentsAsTuple = <T extends readonly AnyComponentCreator[]>(
+    entity: Entity,
+    requires: T,
+): ComponentsFromCreators<T> =>
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- TODO: Refactor to remove this
+    requires.map((r) => entity.get(r)!) as any as ComponentsFromCreators<T>
+
+const entityToQueryResult = <T extends readonly AnyComponentCreator[]>(
+    entity: Entity,
+    query: Query<T>,
+): QueryResult<T> => [getComponentsAsTuple(entity, query.requires), entity]
+
+// TODO: It doesn't seem quite right that we are keeping
+// a reference to entities saved here. Should this class deal
+// only with caching queries and leave the filtering to something else?
+// Also considering moving this to a plugin instead of being used through
+// the Entities class.
+
+export class QueryCache {
+    private entities: Map<EntityId, Entity>
+    private cache = new Map<Query<any>, QueryCacheEntry>()
+
+    constructor(entities: Map<EntityId, Entity>) {
+        this.entities = entities
+    }
+
+    private getOrCreateCacheEntry(query: Query): QueryCacheEntry {
+        const entry = this.cache.get(query)
+
+        if (entry) {
+            return entry
+        }
+
+        const results = this.entities
+            .values()
+            .filter((entity) => entityMatchesQuery(entity, query))
+            .map((entity) => entityToQueryResult(entity, query))
+            .toArray() // TODO: Does this need to be an array or can it be an iterator?
+
+        const newEntry = {
+            results,
+            effects: [],
+        }
+
+        this.cache.set(query, newEntry)
+
+        return newEntry
+    }
+
+    onEntityUpdated(entity: Entity) {
+        this.cache.forEach((cacheEntry, query) => {
+            const { results, effects } = cacheEntry
+
+            const resultIndex = results.findIndex((res) => res[1] === entity)
+
+            if (entityMatchesQuery(entity, query)) {
+                // Entity matches for the first time
+                if (resultIndex < 0) {
+                    const newResult = entityToQueryResult(entity, query)
+                    const newResultIndex = results.push(newResult)
+                    effects.forEach(([effect, cleanups]) => {
+                        const cleanup = effect(newResult)
+                        cleanups[newResultIndex] = cleanup ?? null
+                    })
+                }
+                // TODO: Entity already matched
+            } else {
+                // Entity needs to be removed from results
+                if (resultIndex >= 0) {
+                    results.splice(resultIndex, 1)
+                    effects.forEach(([_, cleanups]) => {
+                        const removedCleanups = cleanups.splice(resultIndex, 1)
+                        // Should only run once (one cleanup is spliced out)
+                        removedCleanups.forEach((cleanup) => {
+                            cleanup?.()
+                        })
+                    })
+                }
+                // Ignore Entities that do not and did not match
+            }
+        })
+    }
+
+
+    addQueryEffect<T extends readonly AnyComponentCreator[]>(
+        query: Query<T>,
+        effect: QueryEffect<T>,
+    ) {
+        const cacheEntry = this.getOrCreateCacheEntry(query)
+
+        ;(cacheEntry as QueryCacheEntry<T>).effects.push([effect, []])
+    }
+
+    query<T extends readonly AnyComponentCreator[]>(query: Query<T>): QueryResult<T>[] {
+        return this.getOrCreateCacheEntry(query).results as QueryResult<T>[]
+    }
+
+    get size() {
+        return this.cache.size
+    }
+}
+/* eslint-enable @typescript-eslint/no-explicit-any -- library code */
+/* eslint-enable @typescript-eslint/consistent-type-assertions -- library code */
