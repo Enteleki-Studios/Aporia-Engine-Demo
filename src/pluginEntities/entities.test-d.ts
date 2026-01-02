a3423bd5b2880d246c29c12e8241c35b7f1a1e96
mike <mikeymaxdb@users.noreply.github.com>
2026-01-01 18:12:22 -0800

    rename queries test

---
+++ src/pluginEntities/entities.test-d.ts
@@ -0,0 +1,43 @@
+import { assertType, describe, expectTypeOf, test } from 'vitest'
+
+import { createComponent } from '@core'
+
+import { Entities } from './entities'
+import type { Entity } from './entity'
+import { createQuery } from './query'
+
+const ComponentA = createComponent('componentA')
+
+const queryA = createQuery([ComponentA])
+
+describe('Entities', () => {
+    test('result components are a proper tuple type', () => {
+        const entities = new Entities()
+
+        entities.addComponents(entities.createEntity(), ComponentA())
+
+        const results = entities.query(queryA)
+
+        results.forEach(([components, entity]) => {
+            expectTypeOf(entity).toEqualTypeOf<Entity>()
+
+            expectTypeOf(components[0]).toEqualTypeOf<ReturnType<typeof ComponentA>>()
+
+            // @ts-expect-error This tuple should only have one element
+            assertType(components[1])
+        })
+    })
+
+    test('query observers receive properly typed results', () => {
+        const entities = new Entities()
+
+        entities.addQueryEffect(queryA, ([components, entity]) => {
+            expectTypeOf(entity).toEqualTypeOf<Entity>()
+
+            expectTypeOf(components[0]).toEqualTypeOf<ReturnType<typeof ComponentA>>()
+
+            // @ts-expect-error This tuple should only have one element
+            assertType(components[1])
+        })
+    })
+})
