6f7b40c1e4c95d652ac77889d63b55d1a63c4ee3
mike <mikeymaxdb@users.noreply.github.com>
2026-01-09 15:08:05 -0800

    added doc to floodWith

--- src/core/utils/array.ts
+++ src/core/utils/array.ts
@@ -4,6 +4,12 @@ export const trimNumberArrayToString = (arr: number[], decimalPlaces = 2): strin
 const averageReducer = (acc: number, num: number) => acc + num
 export const average = (a: number[]) => a.reduce(averageReducer, 0) / a.length
 
+/**
+ * Returns a new array with the elements of the first arg
+ * alternating with the value of the second arg.
+ *
+ * @example floodWith([1, 2, 3], 'a') // [1, 'a', 2, 'a', 3]
+ */
 export const floodWith = <A, B>(a: A[], b: B): (A | B)[] =>
     a.flatMap((element: A, index: number, array: A[]) =>
         index === array.length - 1 ? [element] : [element, b],
