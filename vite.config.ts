07289ee031284ddea46be5bfd2b41213124218a7
mike <mikeymaxdb@users.noreply.github.com>
2026-03-18 22:25:27 -0700

    updated dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import wasm from 'vite-plugin-wasm'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 // import { storybookTest } from '@storybook/addon-vitest/vitest-plugin'
 import react from '@vitejs/plugin-react'
@@ -13,11 +12,12 @@ export default defineConfig({
     build: {
         outDir: './dist',
     },
-    plugins: [tsconfigPaths(), wasm(), react()],
+    plugins: [wasm(), react()],
     server: {
         port: 2080,
     },
     resolve: {
+        tsconfigPaths: true,
         alias: {
             // Added for inspector theme
             '@inspector': path.resolve(__dirname, 'src/inspector'),
